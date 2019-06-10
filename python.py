b28ec1568975f1ac8a57570ab135c32c39d27cd9
22dionse <49483278+22dionse@users.noreply.github.com>
2019-06-10 10:29:58 -0400

    Add files via upload

---
+++ python.py
@@ -0,0 +1,14 @@
+for i in range(10):
+    print("Hello, World")
+
+for i in range(11):
+    print(i)
+    i = i + 1
+
+for x in range(2, 21, 2):
+    print(x)
+
+for y in range(1,11):
+    print(y**2)    
+
+   
